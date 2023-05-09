e86517b7453db83c1cb0182b73ce2e584cd523be
baigalmaa <baigalma@gmail.com>
2023-05-09 10:13:06 +0200

    final

---
+++ app/backend/starting_script.sh
@@ -0,0 +1,12 @@
+npm i --save-dev @nestjs/cli
+npm i --save prisma @prisma/client
+npm i --save @nestjs/jwt passport-jwt
+npm i --save-dev @types/passport-jwt
+npm i --save class-validator class-transformer
+npm i --save @nestjs/config
+npm i --save argon2
+npm i --save passport passport-42
+npm i --save @nestjs/websockets @nestjs/platform-socket.io
+npm i --save @nestjs/platform-express
+npm i --save axios
+npm update
